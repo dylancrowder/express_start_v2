702d2f485cda1e125b5ad416840fdc31b625dcd1
dylan <devdylancrowder@outlook.com>
2025-09-16 19:52:34 -0300

    pino

--- src/utilities/pino.logger.ts
+++ src/utilities/pino.logger.ts
@@ -1,21 +1,24 @@
 import pino from "pino";
 
-const isTestEnv = process.env.NODE_ENV === 'test';
+const isTestEnv = process.env.NODE_ENV === "test";
+const isProdEnv = process.env.NODE_ENV === "production";
 
-const logger = pino({
-  level: isTestEnv ? 'silent' : 'trace', 
-  ...(isTestEnv
-    ? {} 
+const logger = pino(
+  isTestEnv
+    ? { level: "silent" }
+    : isProdEnv
+    ? { level: "trace" } // producción sin pino-pretty
     : {
+        level: "trace", // desarrollo
         transport: {
-          target: 'pino-pretty',
+          target: "pino-pretty",
           options: {
             colorize: true,
-            translateTime: 'yyyy-mm-dd HH:MM:ss',
-            ignore: 'pid,hostname',
+            translateTime: "yyyy-mm-dd HH:MM:ss",
+            ignore: "pid,hostname",
           },
         },
-      }),
-});
+      }
+);
 
 export default logger;
