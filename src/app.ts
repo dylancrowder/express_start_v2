edf30a1b3f49e531f49b7fde42df299256cef4d7
dylan <devdylancrowder@outlook.com>
2025-09-17 11:39:45 -0300

    vercel

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,10 @@ import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/error.middleware";
 import errorRoute from "./middlewares/error.route";
 import monitor from "./middlewares/monitor.middeware";
-import { roleMiddleware } from "./middlewares/rolesMiddleware";
+
 // Routes
 import authRoutes from "./modules/auth/auth.routes";
 import comprasRoutes from "./modules/dashboard/compras/compras.routes";
-import { authMiddleware } from "./middlewares/authMiddleware";
 import { initMongo } from "./db/db_connect";
 
 const app = express();
@@ -29,30 +28,20 @@ initMongo().catch((err) => {
 
 // Configuración de middlewares
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "https://apis.google.com"], // podés agregar los necesarios
-      styleSrc: ["'self'", "https://fonts.googleapis.com"], // sin 'unsafe-inline'
-      fontSrc: ["'self'", "https://fonts.gstatic.com"],
-      imgSrc: ["'self'", "data:"], // permite imágenes inline
-      objectSrc: ["'none'"], // bloquea Flash y otros objetos
-      connectSrc: ["'self'", "https://api.example.com"], // permite conexiones a tu API
-      frameAncestors: ["'none'"], // bloquea el uso de iframes
-    },
+  helmet({
+    contentSecurityPolicy: false,
   })
 );
+
 //CORS
 
-const corsOptions = {
-  origin: ["https://app-gestion-kohl.vercel.app", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-  credentials: true,
-};
-
+app.use(
+  cors({
+    origin: ["https://app-gestion-kohl.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.set("trust proxy", 1);
-app.use(cors(corsOptions));
 app.use(compression());
 app.use(bodyParser.json({ limit: "300kb" }));
 app.use(apiLimiter);
